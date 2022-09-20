6c20c14561940cb2093863ff2890b1a23b8722c0
oderathegreat <oderathegreat@gmail.com>
2022-09-20 10:10:20 +0300

    xml edittext and button plus on create handler

--- app/src/main/java/com/example/sirizetu/MainActivity.kt
+++ app/src/main/java/com/example/sirizetu/MainActivity.kt
@@ -3,21 +3,41 @@ package com.example.sirizetu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+      private lateinit var email_Input:EditText
+      private lateinit var password_Input:EditText
+      private lateinit var btn_CreateUser:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Declare our variables
+
+        email_Input = findViewById(R.id.edtEmail)
+        password_Input = findViewById(R.id.edtPassword)
+        btn_CreateUser = findViewById(R.id.btnCreateUser)
 
         var firebaseDatabase = FirebaseDatabase.getInstance()
         var databaseRef = firebaseDatabase.getReference("messages").push()
 
 
 
+
+
+
+       //on click when user presses button
+        btn_CreateUser.setOnClickListener {
+
+
+        }
 
 
 
