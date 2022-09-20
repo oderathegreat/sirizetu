8deea34439dce9d95ae7dff70b2aef31457ca770
oderathegreat <oderathegreat@gmail.com>
2022-09-20 08:39:24 +0300

    Reading data from a database

--- app/src/main/java/com/example/sirizetu/MainActivity.kt
+++ app/src/main/java/com/example/sirizetu/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.sirizetu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         var firebaseDatabase = FirebaseDatabase.getInstance()
         var databaseRef = firebaseDatabase.reference
-        databaseRef.setValue("Hello There")
+        databaseRef.setValue("My Good People mko vipi")
+
+        //Reading data from our database
+        databaseRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                  var value = snapshot.value
+                Log.d( "Value is ====>: ", value.toString() )
+            }
+
+            override fun onCancelled(error: DatabaseError) {
 
 
+            }
+        })
 
 
     }
