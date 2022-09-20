f7e1f69a52b5a02aac2a686012c3d3b17dfdffec
oderathegreat <oderathegreat@gmail.com>
2022-09-20 09:18:56 +0300

    create a test object to log data in firebase

--- app/src/main/java/com/example/sirizetu/MainActivity.kt
+++ app/src/main/java/com/example/sirizetu/MainActivity.kt
@@ -14,15 +14,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var firebaseDatabase = FirebaseDatabase.getInstance()
-        var databaseRef = firebaseDatabase.reference
+        var databaseRef = firebaseDatabase.getReference("messages")
         databaseRef.setValue("My Good People mko vipi")
+
+        var house_owner = House("Runda", "145 Street oyoo ln", "Kshs 15M","Oyoo tindo")
+
+        //setting value for it to be accessible
+        databaseRef.setValue(house_owner)
 
         //Reading data from our database
         databaseRef.addValueEventListener(object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
 
                   var value = snapshot.value
-                Log.d( "Value is ====>: ", value.toString() )
+                  //Log.d( "Value is ====>: ", value.toString() )
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -33,4 +38,6 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+     //saving objects in db
+    data class House(var location:String, var address:String, var price:String, var owner:String)
 }
