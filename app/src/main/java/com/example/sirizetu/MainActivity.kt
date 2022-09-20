caffde68be940b650252ea11f7b98fd51fcebf66
oderathegreat <oderathegreat@gmail.com>
2022-09-20 09:46:25 +0300

    unecessarry code removed

--- app/src/main/java/com/example/sirizetu/MainActivity.kt
+++ app/src/main/java/com/example/sirizetu/MainActivity.kt
@@ -15,32 +15,13 @@ class MainActivity : AppCompatActivity() {
 
         var firebaseDatabase = FirebaseDatabase.getInstance()
         var databaseRef = firebaseDatabase.getReference("messages").push()
-        databaseRef.setValue("My Good People mko vipi")
 
-        var house_owner = House("Runda Est", "100 Street UN rd", "Kshs 10M","Makori Martin")
 
-        //setting value for it to be accessible
-        databaseRef.setValue(house_owner)
 
-        //Reading data from our database
-        databaseRef.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
 
-                  var value = snapshot.value as HashMap<String, Any>
-                  Log.d( "Value is ====>: ", value.toString() )
-
-                //log a single value
-                //Log.d("Single Value is ", value.get("address").toString())
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Log.d("Error ", error.toString())
-
-            }
-        })
 
 
     }
      //saving objects in db
-    data class House(var location:String, var address:String, var price:String, var owner:String)
+    //data class House(var location:String, var address:String, var price:String, var owner:String)
 }
