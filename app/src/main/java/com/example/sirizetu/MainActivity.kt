22a0c866d249152cdcfcfd48926cf2dbdb91f02b
oderathegreat <oderathegreat@gmail.com>
2022-09-20 08:25:32 +0300

    firebase integration

--- app/src/main/java/com/example/sirizetu/MainActivity.kt
+++ app/src/main/java/com/example/sirizetu/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.sirizetu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var firebaseDatabase = FirebaseDatabase.getInstance()
+        var databaseRef = firebaseDatabase.reference
+        databaseRef.setValue("Hello There")
+
+
+
+
     }
 }
